5598ca9173c7c95beaf280b58b409653e2b85085
skows <sebastian.kowski@lotto24.de>
2017-06-09 09:47:36 +0000

    Put @EnableAutoConfiguiration into application main class and added application.yml for application properties

--- src/main/kotlin/de/lotto24/slotmachine/HelloController.kt
+++ src/main/kotlin/de/lotto24/slotmachine/HelloController.kt
@@ -1,11 +1,9 @@
 package de.lotto24.slotmachine
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@EnableAutoConfiguration
 class HelloController {
 
     @GetMapping(value = "/")
--- src/main/kotlin/de/lotto24/slotmachine/SlotMachineApplication.kt
+++ src/main/kotlin/de/lotto24/slotmachine/SlotMachineApplication.kt
@@ -1,8 +1,10 @@
 package de.lotto24.slotmachine
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+@EnableAutoConfiguration
 @SpringBootApplication
 class SlotMachineApplication
 
