98086e80a04ea4a2bd8d153b7f652e846c7ead83
svoge <sascha.vogel@lotto24.de>
2017-06-09 17:07:42 +0000

    Added more swagger annotations

--- src/main/kotlin/de/lotto24/slotmachine/controller/api/SlotMachineController.kt
+++ src/main/kotlin/de/lotto24/slotmachine/controller/api/SlotMachineController.kt
@@ -1,8 +1,11 @@
 package de.lotto24.slotmachine.controller.api
 
+import de.lotto24.slotmachine.OptionNotFoundException
 import de.lotto24.slotmachine.controller.Storage
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -10,32 +13,36 @@ import org.springframework.web.bind.annotation.*
  * @author skows (initial creation).
  */
 @ApiController
-@Api(description = "Controller that returns a subset of a provided list")
+@Api(description = "Controller that handles options. yeah ha")
 class SlotMachineController {
 
     @Autowired
-    lateinit var storage : Storage
+    lateinit var storage: Storage
 
     @PostMapping("/pick")
     fun <T> pick(@RequestBody pickData: PickData<T>) = pickData.options.pickRandom(pickData.quantity)
 
     @GetMapping("/options/{optionName}")
-    @ApiOperation(value = "Get a list of saved options by optionName")
+    @ApiOperation(value = "Find saved options by optionName", response = Option::class)
+    @ApiResponses(
+            ApiResponse(code = 404, message = "Option not found by optionName", response = OptionNotFoundException::class))
     fun getOptionByName(@PathVariable optionName: String): Option = storage.get(optionName)
 
     @GetMapping("/options")
-    @ApiOperation(value = "Get a list of all saved options", notes = "Returns all saved options")
+    @ApiOperation(value = "Find all saved options", notes = "Returns all saved options")
     fun getOptions(): Collection<Option> = storage.all
 
     @PostMapping("/options")
-    @ApiOperation(value = "Saves options", notes = "Saves options")
+    @ApiOperation(value = "Saves options", notes = "Saves options", response = Option::class)
     fun get(@RequestBody option: Option): Option {
         storage.put(option)
         return option
     }
 
     @DeleteMapping("/options/{optionName}")
-    @ApiOperation(value = "Deletes saved options")
+    @ApiOperation(value = "Deletes saved options", response = Option::class)
+    @ApiResponses(
+            ApiResponse(code = 404, message = "Option not found by optionName", response = OptionNotFoundException::class))
     fun delete(@PathVariable optionName: String): Option = storage.remove(optionName)
 
 }
@@ -44,7 +51,7 @@ class SlotMachineController {
 // Data Class for Input Data
 // ----------------------------------------
 data class PickData<T>(val options: List<T>, val quantity: Int)
-data class Option( val optionName: String, val options: List<String>)
+data class Option(val optionName: String, val options: List<String>)
 
 
 // ----------------------------------------
