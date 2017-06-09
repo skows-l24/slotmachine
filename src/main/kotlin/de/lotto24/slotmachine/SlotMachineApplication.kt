3902eda5abed2379adf81d2fca5358c7a6370e15
svoge <sascha.vogel@lotto24.de>
2017-06-09 07:36:18 +0000

    Created spring boot application

---
+++ src/main/kotlin/de/lotto24/slotmachine/SlotMachineApplication.kt
@@ -0,0 +1,11 @@
+package de.lotto24.slotmachine
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SlotMachineApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SlotMachineApplication::class.java, *args)
+}
---
+++ src/test/kotlin/de/lotto24/slotmachine/SlotMachineApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.lotto24.slotmachine
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SlotMachineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
