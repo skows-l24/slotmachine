1c01602805cdd37b43eb1d80e795253f113dbe7a
Sebastian Kowski <sebastian.kowski@lotto24.de>
2017-12-21 09:49:53 +0100

    Fixed AWS deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 docker --version
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
-eval $(aws ecr get-login --no-include-email --region us-east-1)
+eval $(aws ecr get-login --no-include-email --region us-east-2)
 docker tag hackslot/slot-machine:latest 758649603159.dkr.ecr.us-east-2.amazonaws.com/slotmachine
 docker push 758649603159.dkr.ecr.us-east-2.amazonaws.com/slotmachine:latest
